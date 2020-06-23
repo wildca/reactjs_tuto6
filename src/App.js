b5c78a395ef0f96bb0cfb9758021619c0fb55a2a
traore <tresonance2@gmail.com>
2020-06-23 02:22:27 +0200

    reactjs-tuto6

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Demo from './components/Demo';
+
+
+function App(){
+  return (
+      <div className="App">
+          <h1>App Component</h1>
+          <Demo></Demo>
+      </div>
+  )
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const MyComponent = () => {
+
+  {
+    return <h2 > Way to go you just created a component!! </h2>
+
+    ;
+
+
+  }
+
+}
+ReactDOM.render( < MyComponent / > , document.getElementById('root'));
